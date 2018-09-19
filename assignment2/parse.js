0e4d29bee93feddecd80d7113da3104a826a4fe5
Colleen McCaffrey <colleenmccaffrey@Colleens-MacBook-Pro.local>
2018-09-19 02:05:15 -0400

    added trim method

--- assignment2/parse.js
+++ assignment2/parse.js
@@ -8,7 +8,7 @@ var content = fs.readFileSync('data/text10.txt');
 var $ = cheerio.load(content);
 //console.log($('div >table > tbody > tr > td:nth-child(1)').children().first().contents().text());
 var records = $('div > table > tbody > tr > td:nth-child(1)').contents().not($('div >table > tbody > tr > td:nth-child(1)').children()).map(function () {
-    return $(this).text().trim().replace(/(\r\n|\n|\r)/gm, '').replace(/\s+/g,' ');
+    return $(this).text().replace(/(\r\n|\n|\r)/gm, '').replace(/\s+/g,' ').trim();
   }).get().join('\n');
 
 console.log(records);
