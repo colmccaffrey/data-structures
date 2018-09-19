972fe3ba5ff160898991603d4ebfb730f951d8c4
Colleen McCaffrey <colleenmccaffrey@Colleens-MacBook-Pro.local>
2018-09-19 01:41:31 -0400

    added regex

--- assignment2/parse.js
+++ assignment2/parse.js
@@ -6,17 +6,16 @@ var cheerio = require('cheerio');
 // load the thesis text file into a variable, `content`
 // this is the file that we created in the starter code from last week
 var content = fs.readFileSync('data/text10.txt');
-var addies = [];
 // load `content` into a cheerio object
 var $ = cheerio.load(content);
 // print (to the console) names of thesis students
 //console.log($('div >table > tbody > tr > td:nth-child(1)').children().first().contents().text());
 //console.log($('div >table > tbody > tr > td:nth-child(1)').contents().not($('div >table > tbody > tr > td:nth-child(1)').children()).text());
-var records = $('div >table > tbody > tr > td:nth-child(1)').contents().not($('div >table > tbody > tr > td:nth-child(1)').children()).map(function () {
-    return $(this).text().replace(/[\s,]+/g,' ').trim();
-  }).get().join('\n');
+var records = $('div > table > tbody > tr > td:nth-child(1)').contents().not($('div >table > tbody > tr > td:nth-child(1)').children()).map(function () {
+    return $(this).text().trim().replace(/(\r\n|\n|\r)/gm, '').replace(/\s+/g,' ');
+  }).get();
 
-console.log(records[1]);
+console.log(records);
 //addies = JSON.parse(records);
 
 fs.writeFileSync('data/m10-addresses.txt', records);
