536f4699188e096ba471a43ce3c6a0768ff2f8de
Colleen McCaffrey <colleenmccaffrey@Colleens-MacBook-Pro.local>
2018-09-23 15:17:23 -0400

    removed empty strings

--- assignment2/parse.js
+++ assignment2/parse.js
@@ -2,16 +2,26 @@
 
 var fs = require('fs');
 var cheerio = require('cheerio');
-
+var arr = [];
 var content = fs.readFileSync('data/text10.txt');
 // load `content` into a cheerio object
 var $ = cheerio.load(content);
 //console.log($('div >table > tbody > tr > td:nth-child(1)').children().first().contents().text());
 var records = $('div > table > tbody > tr > td:nth-child(1)').contents().not($('div >table > tbody > tr > td:nth-child(1)').children()).map(function () {
-    return $(this).text().replace(/(\r\n|\n|\r)/gm, '').replace(/\s+/g,' ').trim();
-  }).get().join('\n');
+    return $(this).text().trim().replace(/\s+/g," ");
+  }).get();
 
-console.log(records);
-//addies = JSON.parse(records);
+createAarray(records);
 
-fs.writeFileSync('data/m10-addresses.txt', records);
+function createAarray(recs) {
+    recs = recs.filter(Boolean).join('\n');
+    console.log(recs)
+   createFile(recs);
+}
+
+
+//var addies = records.replace(/(\r\n|\n|\r)/g, '').replace(/\s+/g,' ');
+function createFile(array) {   
+    fs.writeFileSync('data/m10-addresses.json', array);
+    console.log("created");
+}
