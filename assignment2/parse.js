8963611c90e1cf17a91ef434c97464f084c2d423
Colleen McCaffrey <colleenmccaffrey@Colleens-MacBook-Pro.local>
2018-12-12 02:44:13 -0500

    added all data fields

--- assignment2/parse.js
+++ assignment2/parse.js
@@ -2,25 +2,32 @@
 var fs = require('fs');
 var cheerio = require('cheerio');
 var content = fs.readFileSync('data/text10.txt');
-var meetingDT = [];
+var meetingTimes = [];
+var startTimes = [];
+var meetingDt = [];
+var meetingDays = [];
+var newmtgtime = [];
+var newCleanTimes = [];
+var daysTimesMeets = [];
+var specialInt;
 //var content = fs.readFileSync('../assignment1/data/text01.txt');
 
 // load `content` into a cheerio object
 var $ = cheerio.load(content);
 //console.log($('div >table > tbody > tr > td:nth-child(1)').children().first().contents().text());
 var addresses = $('div > table > tbody > tr > td:nth-child(1)').contents().not($('div >table > tbody > tr > td:nth-child(1)').children()).map(function () {
-    return $(this).text().trim().replace(/\s+/g," ");
+    return $(this).text().trim().replace(/\s+/g," ").replace(/'/g, '"');
   }).get();
 var locations = $('div > table > tbody > tr > td:nth-child(1) > h4').map(function () {
-    return $(this).text().trim().replace(/\s+/g," ");
+    return $(this).text().trim().replace(/\s+/g," ").replace(/'/g, '"');
   }).get();
 var names = $('div > table > tbody > tr > td:nth-child(1) > b').map(function () {
-    return $(this).text().trim().replace(/\s+/g," ");
+    return $(this).text().trim().replace(/\s+/g," ").replace(/'/g, '"');
   }).get();
 //add empty .detailsBox div to records without
 $('div > table > tbody > tr > td:nth-child(1)').append('<div class="detailsBox">NA</div>');
 var detailsbox = $('div > table > tbody > tr > td:nth-child(1) > div[class=detailsBox]').map(function () {
-    return $(this).text().trim().replace(/\s+/g," ");
+    return $(this).text().trim().replace(/\s+/g," ").replace(/'/g, '"');
   }).get();
 //add empty span to all records without wheelchair access
 $('div > table > tbody > tr > td:nth-child(1)').append('<span>NA</span>');
@@ -29,19 +36,217 @@ var accessible = $('div > table > tbody > tr > td:nth-child(1) > span').map(func
   }).get();
 $('div > table > tbody > tr > td:nth-child(2) > b').each(function(i, elem) {
     var theDate = ($(elem).text());
-    if (theDate.split(' ')[1] === "From")
+    if (theDate.split(' ')[1] === "From") {
     //console.log("day" + theDate.split(' ')[0]);
     //console.log($(elem).text());
-    meetingDT.push(theDate.split(' ')[0]);
+    var dayOfMeeting = theDate.split(' ')[0];
+    //console.log(dayOfMeeting);
+    }
+    var beginTime = ($(elem));
+   // console.log(" begin time" + beginTime);
+    //meetingDt.push(theDate.split(' ')[0]);
 })
 
-var dayOfWeek = $('div > table > tbody > tr > td:nth-child(2) > b').map(function () {
+//this has all the content in an array- just need to remove the blank spaces and determine which one is a break
+//try cloning an array to get date and then time
+//assign meeting id as you iterate through then serach by meeting id in query to match othe rdetails with meeting times?
+$('div > table > tbody > tr > td:nth-child(2)').each(function(i, elem) {
+    var el = ($(elem).contents()).map(function () {
+    return $(this).text().trim();
+    }).get();
+    
+    //el.forEach(function (meetdeet, index) {
+    //var disOne =  meetdeet.split('</br></br>');
+    //var day = el.split('From')
+    newmtgtime.push(el); 
+    })
+    // //console.log(newmtgtime[0]);
+    newmtgtime.forEach(function (spacedMeeting, index){
+        let cleanmtgtime = spacedMeeting.filter(a => a !== '');
+        newCleanTimes.push(cleanmtgtime);
+    })
+    createMeetingTimes(newCleanTimes);
+
+    function createMeetingTimes(meetingInfo) {
+       //console.log(meetingInfo);
+       //console.log(meetingInfo.length);
+       
+
+       var createAllMeetings = new Object();
+       for (var m = 0; m < meetingInfo.length; m++){
+        var allMeetings = new Array();
+
+        // var numMeetingTimes = Math.ceil(meetingInfo[m].length/8);
+        var numMeetingTimes = meetingInfo[m].length;
+         //console.log(numMeetingTimes);
+        // console.log(meetingInfo[m]);
+        
+
+        for (var n = 0; n < numMeetingTimes; n++){
+        let newArr = meetingInfo[m][n];
+        //console.log(newArr);
+        var thisMeeting = new Object();
+
+        if (newArr.includes("From")){
+        //     console.log("m" + m + "n" + n + " day " + meetingInfo[m][n].split('From')[0]);
+        //     console.log("startHour" + meetingInfo[m][n+1].split('From')[0].split(':')[0]);
+        //     console.log("startMin" + meetingInfo[m][n+1].split(':')[1].split(' ')[0]);
+        //     console.log("amPM" + meetingInfo[m][n+1].split(' ')[1]);
+        //     console.log("type" + meetingInfo[m][n+5]);
+            
+        //     if (meetingInfo[m][n+6] === "Special Interest"){
+        //         console.log("special" + meetingInfo[m][n+7]);
+        //     }
+
+        // }
+        if (meetingInfo[m][n+6] === "Special Interest"){
+            specialInt = meetingInfo[m][n+7];
+        } else {
+             specialInt = "NA";
+        }
+
+            allMeetings.push({
+                    day: meetingInfo[m][n].split('From')[0],
+                    startHour:  meetingInfo[m][n+1].split('From')[0].split(':')[0],
+                    startMin: meetingInfo[m][n+1].split(':')[1].split(' ')[0],
+                    amPM:   meetingInfo[m][n+1].split(' ')[1],
+                    type:   meetingInfo[m][n+5],
+                    specialInterest:  specialInt           
+                })
+         //} 
+         //console.log(thisMeeting);
+            // allMeetings.push(thisMeeting)
+             //console.log(allMeetings);
+            }
+             createAllMeetings =  {
+                meetings: allMeetings
+            }
+
+        }
+            
+        console.log(createAllMeetings.meetings);
+
+       }   //createAllMeetings.push({allMeetings})
+    //    console.log(createAllMeetings.meetings);
+
+    }
+
+    // createAllMeetings = {
+    //     meetings: allMeetings
+
+
+
+//         meetingInfo.forEach(function (dateTime, index){
+//         //console.log("length" + dateTime.length);         
+//         var numMeetingTimes = Math.floor(dateTime.length/6);
+//         //console.log(numMeetingTimes);
+//         //var allMeetings = new Array();
+//         var j = 0;
+//         var s = 0;
+        
+//         for (var i = 0; i <= numMeetingTimes; i ++ ){
+//             //var thisMeeting = new Array();
+//             //var infoString = dateTime.join();
+
+//             //console.log(infoString);
+
+
+//            //console.log(dateTime);
+//                 thisMeeting.push({
+//                     meetings: {
+//                         //day: dateTime.split('From')[0],
+//                         //startHour: dateTime.split(':')[0]
+
+//                     } 
+//                 })
+            
+//                 // thisMeeting = { 
+//                 //     day: dateTime[0 + s + j].split(' ')[0],
+//                 //     startHour:  dateTime[1 + s + j].split(':')[0],
+//                 //     startMin: dateTime[1 + s + j].split(':')[1].split(' ')[0],
+//                 //     amPM:   dateTime[1 + s + j].split(' ')[1],
+//                 //     type:   dateTime[5 + s + j],
+//                 //     specialInterest:  + checkData(dateTime[6 + s + j], dateTime[7 + s + j])
+                
+//                 // }
+//                 //figure out how to set special interest if it doesnt exist and not fuck everything else up
+//                 if (dateTime[6 + s + j] === "Special Interest") {
+//                     s = s + 2;
+//                     j = j + 6;
+
+//                 } else {
+//                     s = s;
+//                     j = j + 6;
+//                 }
+                
+               
+//                 //console.log(thisMeeting);
+//             }
+//             // allMeetings.push({
+//             //     meetings:  thisMeeting
+//             // });
+//             daysTimesMeets.push(thisMeeting);
+
+//             //need to write a function to check for more than one meeting time for each meeting and push into same object for each m
+//          }) 
+
+//     }
+
+//     //console.log("meetings" + daysTimesMeets);
+
+
+// function checkData(info, special) {
+//    if (info === "Special Interest" ) {
+//         //console.log(info);
+//         return special;
+//     } else {
+//         //console.log("na");
+//         return "NA";
+//     }
+// }
+/*
+var dayOfWeek = $('div > table > tbody > tr > td:nth-child(2) > b').map(function () { .children()).map(function () {
+    return $(this).text().trim().replace(/\s+/g," ").replace(/'/g, '"');
     return $(this).text().trim().replace(/\s+/g," ");
   }).get();
-    console.log(meetingDT);
-    createAarray(addresses, locations, names, detailsbox, accessible, dayOfWeek, meetingDT);
+  
+    //console.log(meetingDt);
 
-function createAarray(recs, locs, meetingNames, deets, access, dayweek, meetDT) {
+//splits the information for meetings
+$('div > table > tbody > tr > td:nth-child(2)').each(function(i, elem) {
+    var beginTime = ($(elem).contents());
+    //if (beginTime.split(' ')[1] === "From")
+    //console.log("day" + theDate.split(' ')[0]);
+    //console.log(beginTime[i]);
+    meetingTimes.push(beginTime);
+    
+    })
+    //console.log(meetingTimes);
+    meetingTimes.forEach(function (index, daysTimes){
+        var meetingDeets = daysTimes;
+        //console.log(meetingDeets);
+    //var eachMeetingTime = beginTime.split('<br /> <br />');
+    //console.log(eachMeetingTime);
+   // eachMeetingTime.forEach(function (meetingTime, index) {
+        //console.log(meetingTime[0])
+        //if (meetingTime !== "") {
+            //var dayMeetingTime = meetingTime.split(',');
+            //console.log("meeting times " + dayMeetingTime[0]);
+           // }
+    //})
+})
+
+       // meetingDays.push(beginTime.split(',')[0]);
+
+    //startTimes.push(beginTime.split(' ')[1])
+
+     //console.log("meeting days " + dayOfMeeting);
+    */
+
+    
+createAarray(addresses, locations, names, detailsbox, accessible, meetingDt);
+
+function createAarray(recs, locs, meetingNames, deets, access, meetDay) {
    // var recsCity = [];
 
     var cleanRecs = [];
@@ -80,11 +285,11 @@ function createAarray(recs, locs, meetingNames, deets, access, dayweek, meetDT) 
             }
             recs.splice(j, 1);
         } else {
-            deetRecs.push("");
+            deetRecs.push("NA");
         }
         buildings.push(locs[i]);
         mtgNames.push(meetingNames[i]);
-        if (deets[i] !== "NA") {
+        if (deets[i] !== "") {
             deets.splice(j, 1);
         }
         mtgDetails.push(deets[i]);
@@ -93,7 +298,7 @@ function createAarray(recs, locs, meetingNames, deets, access, dayweek, meetDT) 
             access.splice(j, 1);
         }
         haccess.push(access[i]);
-        daywk.push(dayweek[i]);
+        daywk.push(meetDay[i]);
         //console.log(daywk);
     }
 
